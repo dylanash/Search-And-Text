da26445b97d76befbf553ad1bf0f1d017c5002a9
Dylan McBurnett <Dylan.McBurnett@gmail.com>
2018-03-20 16:19:00 -0400

    add axios

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,41 @@
 import React, {Component} from 'react';
 import keys from '../keys';
-import Bing from 'node-bing-api';
+// import Bing from 'node-bing-api';
+// var Bing = require('bing-search-api');
+// import grabber from 'bing-grabber';
+import axios from 'axios';
 
 class Search extends Component {
 
     constructor() {
         super();
         this.state = {
-            keyword: ''
+            keyword: '',
+            searchTerm: '',
+            searchUrl: '',
+            searchImage: ''
         }; 
         this.inputChange = this.inputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        // this.searchDuck = this.searchDuck.bind(this);
+    }
+
+    searchDuck(term) {
+    
+        axios.get(`https://api.duckduckgo.com/?q=${term}&format=json`)
+        .then((res) => {
+            console.log("RESSSS: ", res.data);
+            if (res.data.Heading) {
+                // let = theTerm = res.data.Heading;
+                console.log("Term: ", res.data.AbstractURL)
+                this.setState({searchTerm: res.data.Heading})
+                this.setState({searchUrl: res.data.AbstractURL})
+            }
+        } )
+        .catch((err) =>{
+            console.error(err);
+        } )
+
     }
 
     inputChange(event) {
@@ -21,17 +46,36 @@ class Search extends Component {
     handleSubmit(event) {
         // event.preventDefault();
         event.preventDefault();
-        console.log("Event.target: ", event);
+        console.log("key", this.state.keyword);
+        let keyword = this.state.keyword
         // console.log(this.state.keyword);
-        this.setState({keyword:''});
-        
-        
+        // if (this.state.keyword.length > 0) {
+            this.searchDuck(keyword);
 
+        // }
+        // this.setState({keyword:''});
+
+    }
+
+    componentDidMount() {
+
+        // axios.get(`https://api.duckduckgo.com/?q=kayaking&format=json`)
+        // .then((res) => {
+        //     console.log("TEST: ", res.data);
+        //     if (res.data.Heading) {
+        //         // let = theTerm = res.data.Heading;
+        //         console.log("Term: ", res.data.Heading)
+        //         // this.setState({searchTerm:theTerm})
+        //     }
+        // } )
+        // .catch((err) =>{
+        //     console.error(err);
+        // } )
     }
 
 
     render() {
-        console.log("KEYS: ", keys);
+        console.log("KEYS: ", keys.accKey);
 
 
         return (
@@ -48,6 +92,10 @@ class Search extends Component {
                     <button className="search-button" 
                             >Clickity Doo Dah</button>
                 </form>
+                <div className="search-results">Search Results<br />
+                    <span>{this.state.searchTerm ? this.state.searchTerm : ''}</span>
+                    <span>{this.state.searchUrl ? this.state.searchUrl : ''}</span>
+                </div>
             </div>
         );
     }
